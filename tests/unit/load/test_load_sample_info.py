dc289d5b939c335f913b7a1cc83dc8eec081fbbf
denisakkavim <43851658+denisakkavim@users.noreply.github.com>
2022-07-09 22:27:02 +0100

    make single assert

--- tests/unit/load/test_load_sample_info.py
+++ tests/unit/load/test_load_sample_info.py
@@ -8,5 +8,7 @@ def test_load_sample_info():
     path = str(Path(__file__).resolve().parent / "test_files" / "sample_details.csv")
     sample_info = load_sample_info(path)
 
-    assert all([isinstance(x, list) for x in sample_info["sample_in_times"]])
-    assert all([isinstance(x, list) for x in sample_info["sample_from_times"]])
+    assert all(
+        [isinstance(x, list) for x in sample_info["sample_in_times"]]
+        + [isinstance(x, list) for x in sample_info["sample_from_times"]]
+    )
