d22d359dc80ee40ef0f5cf95036f336755d82637
denisakkavim <43851658+denisakkavim@users.noreply.github.com>
2022-07-11 21:01:53 +0100

    add cli

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='sample-detection',
+    name="sample-detection",
     packages=find_packages(),
-    version='0.1.0',
-    description='Detecting samples in music',
-    author='Denis Akkavim',
-    license='MIT',
+    version="0.1.0",
+    description="Detecting samples in music",
+    author="Denis Akkavim",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "sample-detection=sample_detection.cli.cli:cli",
+        ]
+    },
 )
