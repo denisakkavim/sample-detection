419ad276290875e016de5d110aa23e3a261cf918
denisakkavim <43851658+denisakkavim@users.noreply.github.com>
2022-07-20 20:15:06 +0100

    fix broken method chaining

--- sample_detection/detect/embedding_generators/audioclip/generator.py
+++ sample_detection/detect/embedding_generators/audioclip/generator.py
@@ -18,23 +18,21 @@ class AudioCLIPEmbeddingGenerator(EmbeddingGenerator):
         super().__init__(sample_duration=sample_duration, sample_rate=sample_rate)
 
         self.logger = logging.getLogger(__name__)
-        self.model = (
-            ESResNeXtFBSP(
-                n_fft=2048,
-                hop_length=561,
-                win_length=1654,
-                window="blackmanharris",
-                normalized=True,
-                onesided=True,
-                spec_height=-1,
-                spec_width=-1,
-                num_classes=1024,
-                apply_attention=True,
-                pretrained=False,
-            )
-            .load_state_dict(torch.load(embedding_model_path))
-            .eval()
+        self.model = ESResNeXtFBSP(
+            n_fft=2048,
+            hop_length=561,
+            win_length=1654,
+            window="blackmanharris",
+            normalized=True,
+            onesided=True,
+            spec_height=-1,
+            spec_width=-1,
+            num_classes=1024,
+            apply_attention=True,
+            pretrained=False,
         )
+        self.model.load_state_dict(torch.load(embedding_model_path))
+        self.model.eval()
 
     def generate_embedding(self, audio_array):
 
