4eda10b58d1db5a13bb186161e915f56a97c6f38
denisakkavim <43851658+denisakkavim@users.noreply.github.com>
2022-07-24 21:42:49 +0100

    merge sample loader into base embedding generator

--- sample_detection/detect/embedding_generators/base.py
+++ sample_detection/detect/embedding_generators/base.py
@@ -1,34 +1,121 @@
-from abc import ABC, abstractmethod
-import numpy as np
-import pandas as pd
+import librosa
 import logging
+import numpy as np
+import os
+import pandas as pd
+import warnings
 
-from typing import Optional, Tuple, Dict
-
-from sample_detection.detect.sample_loader import SampleLoader
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Tuple, Dict, Union
 
 
 class EmbeddingGenerator(ABC):
     def __init__(
         self,
+        sample_rate: int,
         sample_duration: int = 15,
-        sample_rate: Optional[int] = None,
-        sample_loader: Optional[SampleLoader] = None,
     ):
+
         self.logger = logging.getLogger(__name__)
         self.logger.propagate = True
 
-        if sample_loader is None:
-            if sample_rate is None:
-                self.sample_loader = SampleLoader(
-                    sample_duration=sample_duration, sample_rate=16000
-                )
-            else:
-                self.sample_loader = SampleLoader(
-                    sample_duration=sample_duration, sample_rate=sample_rate
-                )
-        else:
-            self.sample_loader = sample_loader
+        self.sample_rate = sample_rate
+        self.sample_duration = sample_duration
+
+    def _load_audio(self, audio_path: str, start_time: int) -> np.ndarray:
+
+        """Load an audioclip.
+
+        :param audio_path: Path to file containing the audio for the sample to be loaded.
+        :type audio_path: str
+        :param start_time: Start time (in seconds) of the sample
+        :type start_time: int
+
+        :raises e: ValueError, FileNotFoundError
+        :return: np.ndarray representation of the audio in audioclip
+
+        """
+
+        if isinstance(audio_path, Path):
+            audio_path = str(audio_path)
+
+        self.logger.info(
+            f"Loading audio from file at {audio_path}, between {start_time}s and {start_time + self.sample_duration} seconds"
+        )
+        try:
+            audio_array, _ = librosa.load(
+                path=audio_path,
+                sr=self.sample_rate,
+                offset=start_time,
+                duration=self.sample_duration,
+            )
+            self.logger.info(
+                f"Successfully loaded audio from file at {audio_path}, between {start_time}s and {start_time + self.sample_duration} seconds"
+            )
+            return audio_array
+        except (ValueError, FileNotFoundError) as e:
+            self.logger.info(
+                f"Could not load audio from file at {audio_path}, between {start_time}s and {start_time + self.sample_duration} seconds"
+            )
+            raise e
+
+    def load_sample(
+        self, audio_dir: str, sample: pd.Series
+    ) -> Tuple[bool, Dict[str, Union[None, Dict[int, np.ndarray]]]]:
+
+        """Load audio for both songs in a sample.
+
+        :param sample: Row from sample info dataframe (as scraped by scraper), with sample times as lists
+        :type sample: pd.Series
+        :param audio_dir: Directory containing the audio files scraped by the scraper.
+        :type audio_dir: str
+
+        :return: Whether the sample was loaded successfully, and a nested dictionary of
+        embeddings, with filenames and sample start times as keys, and audio arrays as values.
+        """
+
+        self.logger.info(f'Loading sample with whosampled_id {sample["whosampled_id"]}')
+
+        if len(sample["sample_in_times"]) != len(sample["sample_from_times"]):
+            self.logger.warning(
+                f'Sample with whosampled id {sample["whosampled_id"]} could not be loaded and will not be used. Reason: Could not find a one-to-one match between sample instances. '
+            )
+            return False, None
+
+        try:
+            with warnings.catch_warnings():
+                audio_dict = {
+                    sample["sample_from_ytid"]: {
+                        time: self._load_audio(
+                            audio_path=os.path.join(
+                                audio_dir, f'{sample["sample_from_ytid"]}.mp3'
+                            ),
+                            start_time=time,
+                        )
+                        for time in sample["sample_from_times"]
+                    },
+                    sample["sample_in_ytid"]: {
+                        time: self._load_audio(
+                            audio_path=os.path.join(
+                                audio_dir, f'{sample["sample_in_ytid"]}.mp3'
+                            ),
+                            start_time=time,
+                        )
+                        for time in sample["sample_in_times"]
+                    },
+                }
+
+            self.logger.info(
+                f'Sample with whosampled_id {sample["whosampled_id"]} successfully loaded '
+            )
+            return True, audio_dict
+
+        except (ValueError, FileNotFoundError):
+            self.logger.warning(
+                f'Sample with whosampled_id {sample["whosampled_id"]} could not be loaded and will not be used. Reason: Could not load sample instance. '
+            )
+            return False, None
 
     @abstractmethod
     def generate_embedding(self, audio_array: np.ndarray) -> np.ndarray:
@@ -60,6 +147,9 @@ class EmbeddingGenerator(ABC):
         self.logger.info(f"Generating embeddings for audio in {audio_dir}")
 
         bad_samples = set()
+        embeddings = dict()
+
+        """
         embeddings = {
             youtube_id: {}
             for youtube_id in (
@@ -67,10 +157,11 @@ class EmbeddingGenerator(ABC):
                 | set(sample_info["sample_in_ytid"])
             )
         }
+        """
 
         for _, row in sample_info.iterrows():
 
-            sample_good, sample_audio = self.sample_loader.load_sample(
+            sample_good, sample_audio = self.load_sample(
                 audio_dir=audio_dir, sample=row
             )
 
@@ -85,9 +176,17 @@ class EmbeddingGenerator(ABC):
                     }
 
                 for ytid in sample_embeddings:
-                    embeddings[ytid] = {**embeddings[ytid], **sample_embeddings[ytid]}
+                    embeddings[ytid] = {
+                        **embeddings.get(ytid, dict()),
+                        **sample_embeddings.get(ytid, dict()),
+                    }
             else:
                 bad_samples.add(row["whosampled_id"])
+
+        # check that all ytids have associated embeddings, and remove any that don't:
+        for key, value in embeddings.items():
+            if not value:
+                embeddings.pop(key)
 
         sample_info = sample_info[~sample_info["whosampled_id"].isin(bad_samples)]
 
