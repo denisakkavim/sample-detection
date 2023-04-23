5d861ef64d1ca0c75c116d92eca99c955d916c58
denisakkavim <43851658+denisakkavim@users.noreply.github.com>
2023-04-23 18:17:18 +0100

    fix bug in load

--- sample_detection/scrape/load.py
+++ sample_detection/scrape/load.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 
 def load_sample_info(sample_info_path: Union[str, Path]) -> pd.DataFrame:
-
     """Load sample info from csv, with sample times as lists (as expected by other
     functions in this library).
 
@@ -16,10 +15,6 @@ def load_sample_info(sample_info_path: Union[str, Path]) -> pd.DataFrame:
     :return: Sample info (as scraped from scraper), with sample times as lists
     :rtype: pd.DataFrame
     """
-
-    if isinstance(sample_info_path, Path):
-        path = str(path)
-
     df = pd.read_csv(sample_info_path)
     df["sample_in_times"] = df["sample_in_times"].apply(literal_eval)
     df["sample_from_times"] = df["sample_from_times"].apply(literal_eval)
